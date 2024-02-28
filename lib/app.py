03f45a4adc000ca382c0c35e030f7e065f23b917
Priscilla-22 <priscillamw123@gmail.com>
2024-02-28 20:53:58 +0300

    completed

---
+++ lib/app.py
@@ -0,0 +1,12 @@
+"""print('Hello World!')
+print("Hello sun!")
+print("Hello sky!")
+
+# Choosing your own print() Ending
+print("Hello world!", end=" ")
+print("Hello sun!", end="!! ")
+print("Hello sky!", end="!!!\n")
+"""
+print('Hello World!', end =' ')
+print('Pass this test, please.')
+
